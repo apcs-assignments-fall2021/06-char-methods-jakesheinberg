16b216a8318a4e525872ad897f240729d26bc9b8
jakesheinberg <23jsheinberg@athenian.org>
2021-09-16 20:52:33 -0700

    I think my code is right but the method headings are being weird

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,28 +4,50 @@ public class MyMain {
     // If the char is not an uppercase letter, 
     // it is returned unchanged. 
     public static char toLower(char ch) {
-        // REPLACE WITH YOUR CODE
-        return '!';
+        if(ch>=97&&ch<=122){
+            int intval = ch-32;
+            char charval = (char) intval;
+            return charval;
+        }else{
+            char intval = ch;
+            return intval;
+        }
     }
 
     // Changes a lowercase letter to an uppercase letter
     // If the char is not a lowercase letter,
     // it is returned unchanged.
     public static char toUpper(char ch) {
-        // REPLACE WITH YOUR CODE HERE
-        return '!';
+        if(ch>=65&&ch<=90){
+            int intval = ch+32;
+            char charval = (char) intval;
+            return charval;
+        }else{
+            char intval = ch;
+            return intval;
     }
 
     // The method is given a String as input and returns a
     // new String where all of the numbers have been removed
     // from the original String.
     public static String removeNumbers(String str) {
-        // REPLACE WITH YOUR CODE
-        return "!!!";
+        String numberLess = "";
+        lenStr = str.length();
+        for(int x=0; x<lenStr; x++ ){
+            char a = str.charAt(x);
+            if(a>=65 && a<=90){
+                numberLess= numberLess + a;
+            }else if(a>=97&&a<=122){
+                numberLess=numberLess+a;
+            }else{
+                String jake ="The GOAT";
+            }
+        }
+        return numberLess;
     }
 
     // Write some code to test your methods!
     public static void main(String[] args) {
-        // ADD SOME TESTING CODE HERE
+        String x=removeNumbers("jake3");
     }
 }
