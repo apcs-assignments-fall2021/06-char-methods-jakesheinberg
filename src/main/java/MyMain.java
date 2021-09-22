65eb955aebf11394f2adafc9489bc549e894ef0f
jakesheinberg <23jsheinberg@athenian.org>
2021-09-21 20:47:04 -0700

    tests passed

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,8 +4,8 @@ public class MyMain {
     // If the char is not an uppercase letter, 
     // it is returned unchanged. 
     public static char toLower(char ch) {
-        if(ch>=97&&ch<=122){
-            int intval = ch-32;
+        if(ch>=65&&ch<=90){
+            int intval = ch+32;
             char charval = (char) intval;
             return charval;
         }else{
@@ -18,32 +18,33 @@ public class MyMain {
     // If the char is not a lowercase letter,
     // it is returned unchanged.
     public static char toUpper(char ch) {
-        if(ch>=65&&ch<=90){
-            int intval = ch+32;
+        if(ch>=97&&ch<=122){
+            int intval = ch-32;
             char charval = (char) intval;
             return charval;
         }else{
             char intval = ch;
             return intval;
+        }
     }
 
     // The method is given a String as input and returns a
     // new String where all of the numbers have been removed
     // from the original String.
     public static String removeNumbers(String str) {
-        String numberLess = "";
-        lenStr = str.length();
-        for(int x=0; x<lenStr; x++ ){
-            char a = str.charAt(x);
-            if(a>=65 && a<=90){
-                numberLess= numberLess + a;
-            }else if(a>=97&&a<=122){
-                numberLess=numberLess+a;
-            }else{
-                String jake ="The GOAT";
-            }
-        }
-        return numberLess;
+                String numberLess = "";
+                int lenStr = str.length();
+                for(int x=0; x<lenStr; x++ ){
+                    char a = str.charAt(x);
+                    if(a>=65 && a<=90){
+                        numberLess= numberLess + a;
+                    }else if(a>=97&&a<=122){
+                        numberLess=numberLess+a;
+                    }else if (a==32 || a==33){
+                        numberLess=numberLess+a;
+                    }
+                }
+                return numberLess;
     }
 
     // Write some code to test your methods!
